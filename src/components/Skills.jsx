696ab49bc4a8bfeec2c44040db615cf8b5c63cd1
said ejjammaa <eljammaasaid@gmail.com>
2023-07-09 02:47:52 +0100

    ii

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+
+
 
  const Skills = () => {
   return (
-    <section id="skills" className="">
-      Skills
-    
-    </section>
+    <section id="skills" className="min-h-[85vh] lg:min-h-[78vh] flex items-center">
+      
+         </section>
   )
 }
 
